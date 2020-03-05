88704a65a13c3dd998ff5290189162636d75d4a1
Germey <cqc@cuiqingcai.com>
2020-03-06 02:05:07 +0800

    add auth

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM ubuntu
 RUN apt update\
-	&& apt -y install squid
+	&& apt -y install squid\
+	&& apt -y install apache2-utils
 COPY squid.conf /etc/squid/squid.conf
+COPY auth.conf /etc/squid/auth.conf
 WORKDIR /app
 COPY start.sh .
 RUN chmod +x start.sh
--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-echo "$PEERS_CONF" > /etc/squid/peers.conf
+echo "$PEERS_CONF" >/etc/squid/peers.conf
+if [ "$PROXY_AUTH" = true ]; then
+  htpasswd -cb /etc/squid/passwd "${PROXY_USERNAME}" "${PROXY_PASSWORD}"
+  mv /etc/squid/auth.conf /etc/squid/squid.conf
+fi
 service squid start
 tail -f /dev/null
